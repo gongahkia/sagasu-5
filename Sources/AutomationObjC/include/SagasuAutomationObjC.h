8bc3ad8561a5b6517318225fbdf10a8236be4ccd
gongahkia <angryapplegravy@gmail.com>
2026-03-14 09:19:21 +0800

    Expose a synchronous WebKit automation session in the Objective-C bridge header.

--- Sources/AutomationObjC/include/SagasuAutomationObjC.h
+++ Sources/AutomationObjC/include/SagasuAutomationObjC.h
@@ -2,3 +2,22 @@
 
 FOUNDATION_EXPORT NSString *SagasuAutomationRuntimeDescription(void);
 FOUNDATION_EXPORT BOOL SagasuAutomationHasCredentialInputs(NSString * _Nullable email, NSString * _Nullable password);
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SagasuAutomationSession : NSObject
+
+- (instancetype)init;
+- (BOOL)loadURLString:(NSString *)urlString timeout:(NSTimeInterval)timeout error:(NSError * _Nullable * _Nullable)error;
+- (nullable NSString *)currentURLString;
+- (nullable id)evaluateJavaScriptSync:(NSString *)script error:(NSError * _Nullable * _Nullable)error;
+- (BOOL)waitForJavaScriptCondition:(NSString *)script
+                           timeout:(NSTimeInterval)timeout
+                      pollInterval:(NSTimeInterval)pollInterval
+                             error:(NSError * _Nullable * _Nullable)error;
+- (BOOL)waitForURLMatching:(NSString *)pattern timeout:(NSTimeInterval)timeout error:(NSError * _Nullable * _Nullable)error;
+- (void)sleepForMilliseconds:(NSInteger)milliseconds;
+
+@end
+
+NS_ASSUME_NONNULL_END
