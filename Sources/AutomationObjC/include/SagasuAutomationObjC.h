79c35dca4da56aae259402b97c7be5684e08cb24
gongahkia <angryapplegravy@gmail.com>
2026-03-14 09:07:24 +0800

    Add the Objective-C bridge header for the automation runtime.

---
+++ Sources/AutomationObjC/include/SagasuAutomationObjC.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSString *SagasuAutomationRuntimeDescription(void);
+FOUNDATION_EXPORT BOOL SagasuAutomationHasCredentialInputs(NSString * _Nullable email, NSString * _Nullable password);
