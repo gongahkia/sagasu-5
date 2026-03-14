04b8356546ceb5db86e8aadcf2fce5badb9e2909
gongahkia <angryapplegravy@gmail.com>
2026-03-14 10:05:37 +0800

    Detach the test target from the macOS helper module.

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
         ),
         .testTarget(
             name: "SagasuTests",
-            dependencies: ["SagasuShared", "SagasuCore", "SagasuHelper"],
+            dependencies: ["SagasuShared", "SagasuCore"],
             path: "Tests/SagasuTests"
         )
     ]
